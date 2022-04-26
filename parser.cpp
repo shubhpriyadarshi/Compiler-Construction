88b5b6847389e518b21491886cde04cc3a4b8642
Subh <shubhpriyadarshi17@gmail.com>
2022-04-26 14:33:45 +0530

    Update parser and lexer

--- parser.cpp
+++ parser.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include <fstream>
 #include "lexer.cpp"
 
+map<pair<int, string>, pair<char, int>> populate_parse_table()
+{
+    map<pair<int, string>, pair<char, int>> mp;
+    mp[{0, "main"}] = {'r', 5};
+    mp[{2, "main"}] = {'s', 9};
+    mp[{3, "main"}] = {'r', 5};
+    mp[{10, "main"}] = {'r', 4};
+    mp[{70, "main"}] = {'r', 3};
+    mp[{9, "("}] = {'s', 12};
+    mp[{11, "("}] = {'s', 13};
+    mp[{23, "("}] = {'s', 38};
+    mp[{25, "("}] = {'s', 39};
+    mp[{26, "("}] = {'s', 40};
+    mp[{27, "("}] = {'s', 41};
+    mp[{29, "("}] = {'s', 46};
+}
+
 // parser to call the lexer function
-int main() {
+int main()
+{
     scanTokens();
+
+    // structure of algorithm
+    /*
+    stack<string> st;
+    |    |
+    | 0  |<-- INITIAL STACK
+    |_$__|
+    while(Scan the tokens){
+        if(mp.find({stoi(st.top()),token}).find()==mp.end()){
+            Report and call Errror routine
+        }
+        else{
+            GOTO and perform Shift/Reduce action
+        }
+    }
+    */
+
     return 0;
 }
