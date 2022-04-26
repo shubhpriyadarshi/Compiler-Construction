d16235c18a52ac611e13b49f52f5eb7e6a81be7c
Subh <shubhpriyadarshi17@gmail.com>
2022-04-26 14:39:00 +0530

    Update lexer and parser

--- lexer.cpp
+++ lexer.cpp
@@ -14,6 +14,7 @@ struct token
 
 vector<token> token_Set;
 vector<string> keywords = {"main", "int", "float", "string", "bool", "for", "while", "return", "var", "if", "elsif", "else", "print"};
+map<int, string> symbol_table;
 
 static token newtk(int tk_num, string lex, int line)
 {
@@ -68,11 +69,13 @@ void scanRelOp(string line, int *idx, int lc)
         if (line[*idx] == '=')
         {
             token_Set.push_back(newtk(600, "<=", lc));
+            symbol_table[600] = "<=";
         }
         else
         {
             (*idx)--;
             token_Set.push_back(newtk(601, "<", lc));
+            symbol_table[601] = "<";
         }
         break;
     case '>':
@@ -80,11 +83,13 @@ void scanRelOp(string line, int *idx, int lc)
         if (line[*idx] == '=')
         {
             token_Set.push_back(newtk(602, ">=", lc));
+            symbol_table[602] = ">=";
         }
         else
         {
             (*idx)--;
             token_Set.push_back(newtk(603, ">", lc));
+            symbol_table[603] = ">";
         }
         break;
     case '=':
@@ -92,6 +97,7 @@ void scanRelOp(string line, int *idx, int lc)
         if (line[*idx] == '=')
         {
             token_Set.push_back(newtk(604, "==", lc));
+            symbol_table[604] = "==";
         }
         else if (line[*idx] == ' ')
         {
@@ -116,6 +122,7 @@ void scanAssignOp(string line, int *idx, int lc)
     if (line[*idx] == '=')
     {
         token_Set.push_back(newtk(700, ":=", lc));
+        symbol_table[700] = ":=";
     }
     else if (line[*idx] == ' ')
     {
@@ -177,7 +184,8 @@ void scanNumberToken(string line, int *idx, int lc)
         }
         else
         {
-            token_Set.push_back(newtk(101, lexeme, lc));
+            token_Set.push_back(newtk(100, lexeme, lc));
+            symbol_table[100] = "num";
         }
     }
     if (state == 2)
@@ -196,6 +204,7 @@ void scanNumberToken(string line, int *idx, int lc)
         else
         {
             token_Set.push_back(newtk(100, lexeme, lc));
+            symbol_table[100] = "num";
         }
     }
     if (state == 3)
@@ -222,6 +231,7 @@ void scanNumberToken(string line, int *idx, int lc)
             (*idx)++;
         }
         token_Set.push_back(newtk(101, lexeme, lc));
+        symbol_table[101] = "num";
     }
 }
 
@@ -248,14 +258,15 @@ void scanNamesToken(string line, int *idx, int lc)
         if (lexeme == keywords[i])
         {
             token_Set.push_back(newtk(501 + i, lexeme, lc));
+            symbol_table[501 + i] = keywords[i];
             flg = 1;
             break;
         }
     }
     if (flg == 0)
     {
-
         token_Set.push_back(newtk(500, lexeme, lc));
+        symbol_table[500] = "id";
     }
 }
 
@@ -272,6 +283,7 @@ void scanStringliterals(string line, int *idx, int lc)
         (*idx)++;
     }
     token_Set.push_back(newtk(111, lexeme, lc));
+    symbol_table[111] = "str";
     (*idx)++;
 }
 
@@ -279,8 +291,8 @@ int scanTokens()
 {
     ifstream fin;  // input file stream
     ofstream fout; // output file stream
-    fin.open("C:/Users/BITS-PC/Desktop/Compiler Project/TC/tc_7.txt");
-    fout.open("C:/Users/BITS-PC/Desktop/Compiler Project/TC/tc_7_op.txt");
+    fin.open("C:/Users/BITS-PC/Desktop/Compiler Project/TC/tc_2.txt");
+    fout.open("C:/Users/BITS-PC/Desktop/Compiler Project/TC/tc_2_op.txt");
     int lc = 0; // maintains line number count
     string line;
     while (getline(fin, line))
@@ -316,15 +328,19 @@ int scanTokens()
                     {
                     case '+':
                         token_Set.push_back(newtk(300, "+", lc));
+                        symbol_table[300] = "+";
                         break;
                     case '-':
                         token_Set.push_back(newtk(301, "-", lc));
+                        symbol_table[301] = "-";
                         break;
                     case '*':
                         token_Set.push_back(newtk(302, "*", lc));
+                        symbol_table[302] = "*";
                         break;
                     case '/':
                         token_Set.push_back(newtk(303, "/", lc));
+                        symbol_table[303] = "/";
                         break;
                     }
                     idx++;
@@ -347,27 +363,35 @@ int scanTokens()
                     {
                     case '{':
                         token_Set.push_back(newtk(400, "{", lc));
+                        symbol_table[400] = "{";
                         break;
                     case '}':
                         token_Set.push_back(newtk(401, "}", lc));
+                        symbol_table[401] = "}";
                         break;
                     case '(':
                         token_Set.push_back(newtk(402, "(", lc));
+                        symbol_table[402] = "(";
                         break;
                     case ')':
                         token_Set.push_back(newtk(403, ")", lc));
+                        symbol_table[403] = ")";
                         break;
                     case ',':
                         token_Set.push_back(newtk(404, ",", lc));
+                        symbol_table[404] = ",";
                         break;
                     case '[':
                         token_Set.push_back(newtk(405, "[", lc));
+                        symbol_table[405] = "[";
                         break;
                     case ']':
                         token_Set.push_back(newtk(406, "]", lc));
+                        symbol_table[406] = "]";
                         break;
                     case ';':
                         token_Set.push_back(newtk(200, ";", lc));
+                        symbol_table[200] = ";";
                         break;
                     }
                     idx++;
